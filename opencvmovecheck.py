1594bf6c0ce8d4f8d2fff01cd0852f0e8cafd1df
qaprompt <572981033@qq.com>
2023-07-17 14:02:44 +0000

    update opencvmovecheck.py.
    Signed-off-by: qaprompt <572981033@qq.com>

--- opencvmovecheck.py
+++ opencvmovecheck.py
@@ -3,6 +3,7 @@
 # 打开VSCODE下cmd终端
 # 运行安装opencv-python==3.4.1.15和opencv-contrib-python==3.4.1.15：
 # python -m ensurepip --default-pip
+# pip install pip -U
 # pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # pip install opencv-python==3.4.1.15
 # pip install opencv-contrib-python==3.4.1.15
